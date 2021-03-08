f75f9809e1b064e567b93ee9d9fe3df74696f404
super <super@192.168.1.5>
2021-03-08 08:21:41 +0100

    add chat

--- giancorlo_progetto_discordsales/src/giancorlo_progetto_discordsales/chat.java
+++ giancorlo_progetto_discordsales/src/giancorlo_progetto_discordsales/chat.java
@@ -5,10 +5,27 @@
  */
 package giancorlo_progetto_discordsales;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author super
  */
-public class chat {
+public class chat implements Runnable{
+private String nome;
+private ArrayList<Object>messaggi;
+private int indirizzo;
+private ArrayList<utente>membri;
+
+    public chat() {
+    }
+
+    @Override
+    public void run() {
+       
+    }
+    public void stampa(){
+        
+    }
     
 }
